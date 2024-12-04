0e90016ea8fa44e6b38c89cfe1e86f07527b7d81
Limbo <t1029651254@163.com>
2024-12-04 11:33:54 +0800

    去掉了嵌入后的房间信息中的酒店id

--- Embedded.py
+++ Embedded.py
@@ -2,32 +2,34 @@ import time
 
 import pandas as pd
 from pymongo import MongoClient
-#
-# # 1. 加载酒店数据和房间数据
-# # 假设酒店信息存储在 hotels.json 文件中，房间信息存储在 rooms.json 文件中
-# hotels_df = pd.read_csv('./data/csv/hotels.csv')
-# rooms_df = pd.read_csv('./data/csv/rooms.csv')
-#
-# # hotels_df = hotels_df.drop(columns=['id'], errors='ignore')  # 去掉酒店数据中的 'hotel_id'
-# # rooms_df = rooms_df.drop(columns=['id'], errors='ignore')  # 去掉房间数据中的 'room_id'
-#
-#
-# # 2. 将房间信息嵌入到酒店信息中
-# # 假设酒店信息有 'hotel_id' 字段，房间信息有 'hotel_id' 字段，我们可以通过 hotel_id 合并数据
-# hotels_with_rooms = hotels_df.copy()
-#
-# # 创建一个空的 'rooms' 列，用来存储每个酒店的房间列表
-# hotels_with_rooms['rooms'] = None
-#
-# # 通过酒店 ID 将房间信息嵌入到酒店信息中
-# for idx, hotel in hotels_with_rooms.iterrows():
-#     hotel_id = hotel['hotel_id']
-#     # 找出与当前酒店 ID 匹配的所有房间
-#     rooms_for_hotel = rooms_df[rooms_df['hotel_id'] == hotel_id]
-#     # 将房间信息转换为字典并添加到酒店数据中
-#     hotels_with_rooms.at[idx, 'rooms'] = rooms_for_hotel.to_dict(orient='records')
-#
-# hotels_with_rooms.to_json('./data/josn/hotels_with_rooms.json', orient='records', lines=True, force_ascii=False)
+
+# 1. 加载酒店数据和房间数据
+# 假设酒店信息存储在 hotels.json 文件中，房间信息存储在 rooms.json 文件中
+hotels_df = pd.read_csv('./data/csv/hotels.csv')
+rooms_df = pd.read_csv('./data/csv/rooms.csv')
+
+hotels_df = hotels_df.drop(columns=['id'], errors='ignore')  # 去掉酒店数据中的 'hotel_id'
+rooms_df = rooms_df.drop(columns=['id'], errors='ignore')  # 去掉房间数据中的 'room_id'
+
+
+# 2. 将房间信息嵌入到酒店信息中
+# 假设酒店信息有 'hotel_id' 字段，房间信息有 'hotel_id' 字段，我们可以通过 hotel_id 合并数据
+hotels_with_rooms = hotels_df.copy()
+
+# 创建一个空的 'rooms' 列，用来存储每个酒店的房间列表
+hotels_with_rooms['rooms'] = None
+
+# 通过酒店 ID 将房间信息嵌入到酒店信息中
+for idx, hotel in hotels_with_rooms.iterrows():
+    hotel_id = hotel['hotel_id']
+    # 找出与当前酒店 ID 匹配的所有房间
+    rooms_for_hotel = rooms_df[rooms_df['hotel_id'] == hotel_id]
+    # 去掉房间信息中的 'hotel_id' 字段
+    rooms_for_hotel = rooms_for_hotel.drop(columns=['hotel_id'], errors='ignore')
+    # 将房间信息转换为字典并添加到酒店数据中
+    hotels_with_rooms.at[idx, 'rooms'] = rooms_for_hotel.to_dict(orient='records')
+
+hotels_with_rooms.to_json('./data/josn/hotels_with_rooms_noid.json', orient='records', lines=True, force_ascii=False)
 
 
 hotels_with_rooms = pd.read_json("./data/josn/hotels_with_rooms_noid.json", lines=True)
